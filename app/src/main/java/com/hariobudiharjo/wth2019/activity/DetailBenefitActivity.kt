3cbb677f1446aa9db335d235efbf04e35512cd04
Hario Budiharjo <hariobudiharjo@Gmail.com>
2019-07-21 00:34:17 +0700

    structure benefit , plan,konfirmasi,notife,sucsess

---
+++ app/src/main/java/com/hariobudiharjo/wth2019/activity/DetailBenefitActivity.kt
@@ -0,0 +1,13 @@
+package com.hariobudiharjo.wth2019.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hariobudiharjo.wth2019.R
+
+class DetailBenefitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_benefit)
+    }
+}
---
+++ app/src/main/java/com/hariobudiharjo/wth2019/activity/SucsessActivity.kt
@@ -0,0 +1,13 @@
+package com.hariobudiharjo.wth2019.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hariobudiharjo.wth2019.R
+
+class SucsessActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sucsess)
+    }
+}
